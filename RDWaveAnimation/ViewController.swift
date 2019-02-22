4b2dcb27d0a0b820a26cdb7ca5fb6224f6aef656
Yuyu Yang <young_yu@outlook.com>
2019-02-23 03:29:02 +0800

    格式改动

--- RDWaveAnimation/ViewController.swift
+++ RDWaveAnimation/ViewController.swift
@@ -12,11 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         
         let waveView = WaveView(CGRect(x: self.view.bounds.width / 2 - 80, y: self.view.bounds.height / 2 - 80, width: 160, height: 160), word: "YY")
         self.view.addSubview(waveView)
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 }
 
