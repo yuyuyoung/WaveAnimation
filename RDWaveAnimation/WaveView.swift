08f906e0bd65c7ef33e348b8ebe6e54f6419c034
Yuyu Yang <young_yu@outlook.com>
2019-02-23 03:24:07 +0800

    first

---
+++ RDWaveAnimation/WaveView.swift
@@ -0,0 +1,21 @@
+//
+//  WaveView.swift
+//  RDWaveAnimation
+//
+//  Created by yangyu on 2019/2/23.
+//  Copyright © 2019 YangYiYu. All rights reserved.
+//
+
+import UIKit
+
+class WaveView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
